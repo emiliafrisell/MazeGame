327241a402c2b5ef7b02e17d4c9d44d3a196c432
Emilia Frisell <emiliafrisell@gmail.com>
2021-05-28 09:48:09 +0200

    fixed intro logic

--- src/intro.js
+++ src/intro.js
@@ -9,6 +9,13 @@ document.querySelector('#start-button').addEventListener('click', hideInstructio
 
 // })
 
+const openGates = () => {
+    gate1.setAttribute('rotation', '0 90 0')
+    gate1.setAttribute('position', '5.2 -1.6 -7')
+    gate2.setAttribute('rotation', '0 -100 0')
+    gate2.setAttribute('position', '4.9 -1.6 -9.7')
+}
+
 const URL = "./";
 
     let model, webcam, labelContainer, maxPredictions;
@@ -65,21 +72,17 @@ const URL = "./";
         for (let i = 0; i < maxPredictions; i++) {
             if (prediction[i].probability.toFixed(2) > 0.8 && prediction[i].className == 'Coffee') {
                 coffee = true;
-                gate1.setAttribute('rotation', '0 90 0')
-                gate1.setAttribute('position', '5.2 -1.6 -7')
-                gate2.setAttribute('rotation', '0 -100 0')
-                gate2.setAttribute('position', '4.9 -1.6 -9.7')
                 
                 document.getElementById('boxes').setAttribute('visible', 'true');
 
                 setTimeout(() => {  
-                    speak('Thank you, I have now opened the gates for you.')
+                    speak('Thats better, Now if you take these crates with you I will then open the gates.')
                     document.getElementById('camera-container').style = 'display: none;'
                 }, 500)
                 
                 setTimeout(() => {  
                     document.getElementById('speech-bubble').setAttribute('visible', 'false');
-                }, 5000)
+                }, 7000)
                 
                 webcam.stop();
             const classPrediction =
